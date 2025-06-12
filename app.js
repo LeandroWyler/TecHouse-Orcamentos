38f98575e9c1827762755bda72cce2ee06bbd2cb
LeandroWyler <leandrowyller15@gmail.com>
2025-06-12 11:16:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -31,3 +31,8 @@ function buscarModelo() {
     alert('Modelo não encontrado.');
   }
 }
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js")
+    .then(() => console.log("Service Worker registrado com sucesso!"))
+    .catch(err => console.error("Erro ao registrar o Service Worker:", err));
+}
